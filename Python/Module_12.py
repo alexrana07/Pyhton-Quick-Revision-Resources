3f5eb2ff9bfe81104736bba2f8db8c61a46434f7
Alex Rana <ranaalex855@gmail.com>
2026-01-04 17:19:10 +0530

    Update Module_12.py

---
+++ Python/Module_12.py
@@ -0,0 +1,13 @@
+"""
+1. What is a Loop?
+ans:A loop is used to repeat a block of code multiple times until a condition is met.
+
+In simple words:
+Loops help us avoid writing the same code again and again.
+
+2. Types of Loops in Python
+Python mainly has two loops:
+
+for loop
+while loop
+"""
