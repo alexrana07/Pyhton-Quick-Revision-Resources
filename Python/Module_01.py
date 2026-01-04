c6d6f99ea34663a09f1c0fd42cdbfb1f073500de
Alex Rana <ranaalex855@gmail.com>
2026-01-04 19:11:20 +0530

    Update Module_01.py

--- Python/Module_01.py
+++ Python/Module_01.py
@@ -1,62 +1,51 @@
 """
-1.What is Programming?
-ans:Programming is the process of writing instructions that tell a computer what to do in order to solve a problem or perform a task.
+1.Whta is Programming?
+ans:Programming is the process of writing instructions (code) to tell a computer how to perform a task or solve a problem.
 
-A computer cannot think or make decisions on its own.
-It only works according to the instructions given by a programmer.
-These instructions are written using programming languages such as Python, C, Java, or C++.
+2.What is Programming language?
+ans:A programming language is a language used to communicate with a computer and tell it what to do by writing instructions (code).
 
-In simple words:
-Programming is a way of communicating with computers to make them work as we want.
+3.What is Python?
+ans:Python is a high-level, easy-to-learn programming language used to create programs and applications.
 
-2.Why Do We Use Programming?
-ans:We use programming to make computers useful and intelligent.
+4.Why we use Python?
+ans:simple and readable syntax
+it is open-source
+Saves time and effort
+Used in web development, data science, AI, and automation
+Beginner-friendly and powerful
 
-Some important reasons are:
-a. To Automate Tasks : Programming allows computers to perform tasks automatically and repeatedly without human effort.
-b. To Develop Software and Applications : All software and applications are created using programming.
-c. To Solve Real-World Problems : Programming helps solve complex real-life problems efficiently.
-d. To Control Machines and Devices : Programming is used to control electronic devices and machines.
-e. To Work with Data and Technology : Programming helps process, analyze, and manage large amounts of data.
+5.What is compiler?
+ans:A compiler is a software programs that converts source code written in a programming language into machine code, so the computer can understand and execute it.
 
-3.What is a Programming Language?
-ans:A programming language is a formal language used to write instructions that a computer can understand and execute to perform specific tasks.
+6.What us Interpreter?
+ans:An interpreter is a software programs that reads and executes code line by line, converting it into machine language while the program runs.
 
-In simple words:
-A programming language is a way to communicate with a computer and tell it what to do.
+7.Difference between Compiler and Interpreter:
+ans:A compiler translates the whole program at once before running it.
+An interpreter translates and runs the program line by line.
 
-4.Why Do We Need Programming Languages?
-ans:Computers do not understand human languages like English or Hindi.
-They only understand machine language (0s and 1s), which is very difficult for humans.
+8. What is pip?
+ans:pip is a package manager in Python used to install, update, and manage libraries on our system.
+example:pip install django
 
-Programming languages act as a bridge between humans and computers:
-Humans write instructions in a programming language
-The computer converts them into machine language
-The computer performs the task
+for write comments we use "#"
+example: #this is comment
+now this like is become comment and it will not execute on display
+note: for write multi line comments. as i have write all this nots we use triple single or double quotes.
 
-5.What is a Compiler?
-ans:A compiler is a system software that translates an entire program written in a high-level programming language into machine language (0s and 1s) at once, so that the computer can understand and execute it.
 
-In simple words:
-A compiler converts human-written code into computer-understandable code.
-
-6.Why Do We Need a Compiler?
-ans:Computers understand only machine language
-Programmers write code in high-level languages like C, C++, Java
-A compiler acts as a translator between the programmer and the computer
-
-Without a compiler, the computer cannot run high-level programs.
-
-7.What is an Interpreter?
-ans:An interpreter is a system software that translates and executes a program line by line from a high-level programming language into machine language.
-
-In simple words:
-An interpreter reads, translates, and executes the program one line at a time.
-
-8.Why Do We Need an Interpreter?
-ans:Computers understand only machine language
-Programmers write code in high-level languages
-An interpreter works as a translator between the programmer and the computer
-
-Without an interpreter, some programming languages cannot run.
 """
+
+
+# first progrm
+# print() is a Python function used to display output on the screen.and in this program print function will display "hello,world" on terminal
+print("hello,world") 
+
+
+
+
+
+
+
+
