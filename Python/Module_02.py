fa834224f2f2c35682902bcf07a47af1a1b172ef
Alex Rana <ranaalex855@gmail.com>
2026-01-04 18:44:20 +0530

    Update Module_02.py

---
+++ Python/Module_02.py
@@ -0,0 +1,104 @@
+"""
+1.What is a Variable?
+ans:A variable is a name given to a memory location that is used to store data in a program.
+The value stored in a variable can change during program execution.
+
+In simple words:
+A variable is a container used to store information that can change.
+
+Variables are used to:
+Store data
+Reuse values
+Perform calculations
+Make programs flexible
+
+Without variables, programs would be hard-coded and useless.
+
+2.Real-Life Example 
+
+Think of a box with a label:
+Box label → Variable name
+Item inside box → Variable value
+
+Example:
+
+Box named age
+Value inside is 20
+Later, the value can change:
+age = 21
+
+3.Rules of Writing Variables:
+A variable name must follow certain rules so that the computer can understand it correctly.
+
+a.Variable Name Must Start with a Letter or Underscore (_):
+example:
+age
+_name
+total
+
+b.Variable Name Cannot Start with a Number,Numbers can be used after the first letter:
+example:
+student1
+marks2
+
+c.No Spaces Are Allowed in Variable Names
+If you need multiple words, use:
+
+Underscore _
+Camel case
+
+
+example:
+total_marks
+totalMarks
+
+d.No Special Characters Are Allowed
+Do not use symbols like:
+@ # $ % & * !
+
+e.Variable Names Are Case-Sensitive
+Uppercase and lowercase letters are different.
+
+Example:
+age = 20
+Age = 25
+age and Age are two different variables.
+
+f.Keywords Cannot Be Used as Variable Names
+Keywords are reserved words used by the programming language.
+
+if = 10
+for = 5
+class = 3
+
+g.Variable Names Should Be Meaningful
+Choose names that explain the data.
+
+example:
+total_marks
+student_age
+
+4.What is an Identifier?
+ans:An identifier is the name used to identify program elements such as variables, functions, classes, arrays, or objects in a program.
+
+In simple words:
+An identifier is a name given to something in a program so it can be recognized.
+
+Identifiers are needed to:
+Identify data stored in memory
+Access variables and functions
+Make programs readable and understandable
+
+Without identifiers, it would be impossible to refer to data or operations in a program.
+
+Examples of Identifiers:
+age
+total_marks
+studentName
+calculateSum
+
+
+In the above examples:
+age → identifier for a variable
+calculateSum → identifier for a function
+"""
